642197e84bc706de90b47f3438d224b70402c917
kyleung1 <leungkyle2@gmail.com>
2023-08-04 15:00:29 -0700

    env variables

--- script.py
+++ script.py
@@ -1,11 +1,16 @@
 import json
 import certifi
+import os
 from pymongo import MongoClient
 from difflib import get_close_matches
+from dotenv import load_dotenv
 
-client = MongoClient("", tlsCAFile=certifi.where())
+load_dotenv()
+
+client = MongoClient(os.getenv('mongo_uri'), tlsCAFile=certifi.where())
 db = client["chatbot"]
 collection = db["questions"]
 
-collection.insert_one({"question": "", "answer": ""})
+# collection.insert_one({"question": "", "answer": ""})
+collection.insert_one({"question": "how old are you", "answer": "I was born in 2001"})
 client.close()
